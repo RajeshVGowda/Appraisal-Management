9e01f30432079c4fa0449f5da980458029c78403
RajeshVGowda <rajesh.venkateshappa@tesco.com>
2018-06-01 18:37:20 +0530

    First Commit

---
+++ src/main/java/com/nineleaps/AppraisalManagement/AppraisalManagementApplication.java
@@ -0,0 +1,12 @@
+package com.nineleaps.AppraisalManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppraisalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppraisalManagementApplication.class, args);
+	}
+}
